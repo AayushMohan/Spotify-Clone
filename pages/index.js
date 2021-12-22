4e0f9a981430801b84aee0b43650556a25877284
AayushMohan <aayushmohan1702@gmail.com>
2021-12-22 13:22:38 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Sidebar from "../Components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-black h-screen overflow-hidden">
       <main className="">
         <Sidebar />
         {/* Center */}
