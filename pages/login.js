f54c9e95539708ba688b43a6b829a20eb32f60bb
AayushMohan <aayushmohan1702@gmail.com>
2021-12-23 23:16:42 +0530

    commit

--- pages/login.js
+++ pages/login.js
@@ -8,7 +8,7 @@ const Login = ({ providers }) => {
 
       {Object.values(providers).map((provider) => (
         <div>
-          <button>test</button>
+          <button>Login with {provider.name}</button>
         </div>
       ))}
     </div>
