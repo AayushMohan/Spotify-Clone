f82d7add94f837d2c99c950d118b2defbc756c9a
AayushMohan <aayushmohan1702@gmail.com>
2021-12-24 23:26:01 +0530

    commit

--- Components/Center.js
+++ Components/Center.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Center = () => {
   return (
-    <div className="flex-grow">
+    <div className="flex flex-grow">
       <h1>I am a center</h1>
     </div>
   );
