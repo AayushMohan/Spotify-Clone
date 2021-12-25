48b47034e098f0b49978943bff42d9a699495df7
AayushMohan <aayushmohan1702@gmail.com>
2021-12-25 23:41:33 +0530

    commit

--- Components/Center.js
+++ Components/Center.js
@@ -8,7 +8,11 @@ const Center = () => {
       <h1>I am center</h1>
       <header>
         <div>
-          <img src="{session?.user.image}" alt="" />
+          <img
+            className="rounded-full w-190 h-10"
+            src="{session?.user.image}"
+            alt=""
+          />
         </div>
       </header>
     </div>
