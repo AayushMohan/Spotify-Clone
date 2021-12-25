3d22c7a19e5e23f6687e030a47375921b1de8d57
AayushMohan <aayushmohan1702@gmail.com>
2021-12-25 23:37:17 +0530

    commit

--- Components/Center.js
+++ Components/Center.js
@@ -1,6 +1,8 @@
+import { useSession } from "next-auth/react";
 import React from "react";
 
 const Center = () => {
+  const { data: session } = useSession();
   return (
     <div className="flex flex-grow text-white">
       <h1>I am center</h1>
