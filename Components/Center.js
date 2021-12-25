95d6f98fb70ffbd18a6651840cdf728cdeca90d5
AayushMohan <aayushmohan1702@gmail.com>
2021-12-25 23:17:39 +0530

    commit

--- Components/Center.js
+++ Components/Center.js
@@ -4,6 +4,11 @@ const Center = () => {
   return (
     <div className="flex flex-grow text-white">
       <h1>I am center</h1>
+      <header>
+        <div>
+          <img src="" alt=""></img>
+        </div>
+      </header>
     </div>
   );
 };
