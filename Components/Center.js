28b209486fe788503b4a58e19f1ae1f316076e25
AayushMohan <aayushmohan1702@gmail.com>
2021-12-27 01:12:49 +0530

    commit

--- Components/Center.js
+++ Components/Center.js
@@ -42,7 +42,8 @@ const Center = () => {
   return (
     <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white">
+        <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" onClick={signOut}
+        >
           <img
             className="rounded-full w-10 h-10"
             src={session?.user.image}
