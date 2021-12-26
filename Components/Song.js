9570af9e7134474aa595cf4ee09969a99177592a
AayushMohan <aayushmohan1702@gmail.com>
2021-12-27 01:45:40 +0530

    commit

--- Components/Song.js
+++ Components/Song.js
@@ -20,11 +20,10 @@ function Song({ order, track }) {
           </p>
           <p className="w-40">{track.track.artists[0].name}</p>
         </div>
-
-        <div className="flex items-center justify-between ml-auto md:ml-0">
-          <p className="w-40 hidden md:inline">{track.track.album.name}</p>
-          <p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
-        </div>
+      </div>
+      <div className="flex item-center justify-between ml-auto md:ml-0">
+        <p className="w-40 hidden md:inline">{track.track.album.name}</p>
+        <p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
       </div>
     </div>
   );
