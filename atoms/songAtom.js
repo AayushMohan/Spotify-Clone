b92ab90b4abfc1dcc3352af13dd176271b9d7274
AayushMohan <aayushmohan1702@gmail.com>
2021-12-27 01:53:05 +0530

    commit

--- atoms/songAtom.js
+++ atoms/songAtom.js
@@ -1,1 +1,11 @@
 import { atom } from "recoil";
+
+export const currentTrackState = atom({
+  key: "currentTrackState", // unique ID (with respect to other atoms/selectors)
+  default: null, // default value (aka initial value)
+});
+
+export const isPlayingState = atom({
+  key: "isPlayingState",
+  default: false,
+});
