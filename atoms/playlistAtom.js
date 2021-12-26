f018c747b16a76231bc21d8c84719588d32208e0
AayushMohan <aayushmohan1702@gmail.com>
2021-12-26 20:36:47 +0530

    commit

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -1,5 +1,10 @@
 import { atom } from "recoil";
 
+export const playlistAtom = atom({
+  key: "playlistAtomState",
+  default: null,
+});
+
 export const playlistIdState = atom({
   key: "playlistIdState",
   default: "61e0bdce6a9d484e901b0003a1805213",
