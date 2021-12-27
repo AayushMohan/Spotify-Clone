850ab59e8338c76d64942d9da69e88dd5f37ef6b
AayushMohan <aayushmohan1702@gmail.com>
2021-12-27 14:25:02 +0530

    commit

---
+++ Components/Player.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import useSpotify from "../hooks/useSpotify";
+
+function Player() {
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+  return (
+    <div>
+      {/* Left */}
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Player;
