47ce5902a979b4e19b986a6dd1f16d955f577f2d
AayushMohan <aayushmohan1702@gmail.com>
2021-12-27 16:02:51 +0530

    commit

--- Components/Player.js
+++ Components/Player.js
@@ -1,5 +1,5 @@
 import { useSession } from "next-auth/react";
-import React from "react";
+import { useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSpotify from "../hooks/useSpotify";
@@ -7,10 +7,11 @@ import useSpotify from "../hooks/useSpotify";
 function Player() {
   const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
-  const [currentTrackId, setCurrentTrackId] =
+  const [currentTrackId, setCurrentIdTrack] =
     useRecoilState(currentTrackIdState);
 
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [volume, setVolume] = useState(50);
 
   return (
     <div>
